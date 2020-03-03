99af6149e148847e23c2d52dd227d3f2ba4ea1c4
Даниил <D3l0ry@outlook.com>
2020-03-03 16:24:45 +0500

    Add hepler for struct

---
+++ Trion/SDK/Structures/Intrinsics.cs
@@ -0,0 +1,7 @@
+﻿namespace Trion.SDK.Structures
+{
+    internal static class Intrinsics
+    {
+        public static bool IsNull<T>(this T type) where T : unmanaged => typeof(T) == null;
+    }
+}

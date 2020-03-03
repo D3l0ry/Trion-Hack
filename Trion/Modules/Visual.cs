f4bb6952a098385c542fc12919a116cff9b1bc63
Даниил <D3l0ry@outlook.com>
2020-03-03 16:43:35 +0500

    Added Fake Prime, optimized code

--- Trion/Modules/Visual.cs
+++ Trion/Modules/Visual.cs
@@ -1,57 +1,71 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 using Trion.Client.Configs;
+using Trion.SDK.Dumpers;
 using Trion.SDK.Interfaces;
 using Trion.SDK.Interfaces.Client;
 using Trion.SDK.Interfaces.Client.Entity.Structures;
 using Trion.SDK.Surface.Controls;
 using Trion.SDK.Surface.Drawing;
+using Trion.SDK.WinAPI;
+using Trion.SDK.WinAPI.Enums;
 
 namespace Trion.Modules
 {
-    internal unsafe class Visual
+    internal unsafe struct Visual
     {
         #region Variables
         private static readonly Label WaterMarkLabel = new Label()
         {
             ForeColor = Color.FromArgb(152, 241, 221),
             Position = new Point(5, 5),
-            Font = new Font("Tahoma",29, Font.FontFlags.FONTFLAG_OUTLINE),
-            Text = "Trion Project"
+            Font = new Font("Tahoma", 20, Font.FontFlags.FONTFLAG_OUTLINE),
+            Text = "Trion"
         };
+
+        private static IGlowObjectManager* IGlowObjectManager = Offsets.GlowObjectManager;
         #endregion
 
-        public static void GlowRender()
+        #region Private
+        private static byte[] PrimeCode = { 0x74, 0xEB };
+        private static bool PrimeState = false;
+        #endregion
+
+        public static void GlowRender(BasePlayer* localPlayer)
         {
-            BasePlayer* LocalPlayer = Interface.ClientEntityList.GetClientEntity(Interface.VEngineClient.GetLocalPlayer)->GetPlayer;
+            int myTeam = localPlayer->TeamNum;
 
-            for (int Index = 0; Index < Interface.VEngineClient.GetMaxClients; Index++)
+            for (int index = 0; index < IGlowObjectManager->Size; index++)
             {
-                BasePlayer* Entity = Interface.ClientEntityList.GetClientEntity(Index)->GetPlayer;
-
-                if (Entity == null || !Entity->IsAlive)
+                if (index == Interface.VEngineClient.GetLocalPlayer)
                 {
                     continue;
                 }
 
-                if (LocalPlayer->TeamNum == Entity->TeamNum)
+                BasePlayer* entity = Interface.ClientEntityList.GetClientEntity(index)->GetPlayer;
+
+                if (entity == null)
                 {
                     continue;
                 }
 
-                _ = ConfigManager.CVisual.GlowHPEnable ? Entity->GlowObject->Color = new IGlowObjectManager.GlowColor(1 - (Entity->GetHealth / 100f), Entity->GetHealth / 100f, 0, 175 / 255f) : Entity->GlowObject->Color = ConfigManager.CVisual;
+                if (myTeam == entity->TeamNum)
+                {
+                    continue;
+                }
 
-                Entity->GlowObject->fullBloomRender = ConfigManager.CVisual.FullBloom;
-                Entity->GlowObject->glowStyle = ConfigManager.CVisual.GlowStyle;
-                Entity->GlowObject->renderWhenOccluded = true;
+                IGlowObjectManager->SetEntity(index, entity);
+                IGlowObjectManager->SetColor(index, ConfigManager.CVisual.GlowHPEnable, entity->GetHealth);
+                IGlowObjectManager->SetRenderFlags(index, true, false);
             }
         }
 
-        public static void RadarRender(BasePlayer* Entity) => Entity->Spotted = 1;
+        public static void RadarRender(BasePlayer* entity) => entity->Spotted = 1;
 
-        public static void RevealRanks(IClientMode.UserCmd* CMD)
+        public static void RevealRanks(ref IClientMode.UserCmd cmd)
         {
-            if ((CMD->buttons & IClientMode.Buttons.IN_SCORE) != 0)
+            if ((cmd.buttons & IClientMode.Buttons.IN_SCORE) != 0)
             {
                 Interface.BaseClientDLL.DispatchUserMessage(50, 0, 0, (void*)0);
             }
@@ -59,14 +73,34 @@ namespace Trion.Modules
 
         public static void NoFlash()
         {
-            var LocalPLayer = Interface.ClientEntityList.GetClientEntity(Interface.VEngineClient.GetLocalPlayer)->GetPlayer;
+            var localPlayer = Interface.ClientEntityList.GetClientEntity(Interface.VEngineClient.GetLocalPlayer)->GetPlayer;
 
-            if (!LocalPLayer->IsAlive)
+            if(localPlayer == null)
             {
                 return;
             }
 
-            LocalPLayer->FlashMax = 0f;
+            if (!localPlayer->IsAlive)
+            {
+                return;
+            }
+
+            localPlayer->FlashMax = 0f;
+        }
+
+        public static void FakePrime()
+        {
+            if (ConfigManager.CVisual.Prime != PrimeState)
+            {
+                PrimeState = ConfigManager.CVisual.Prime;
+
+                if (NativeMethods.VirtualProtect((IntPtr)Offsets.FakePrime, 1, ProtectCode.PAGE_EXECUTE_READWRITE, out uint Old))
+                {
+                    *Offsets.FakePrime = ConfigManager.CVisual.Prime ? PrimeCode[1] : PrimeCode[0];
+
+                    NativeMethods.VirtualProtect((IntPtr)Offsets.FakePrime, 1, (ProtectCode)Old, out uint nullptr);
+                }
+            }
         }
 
         public static void WaterMark() => WaterMarkLabel.Show();

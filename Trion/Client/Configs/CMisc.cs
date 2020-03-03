7c148c6cf7695c4909c1ca1a28a966040cc2b146
Даниил <D3l0ry@outlook.com>
2020-03-03 16:31:02 +0500

    Add config Misc

---
+++ Trion/Client/Configs/CMisc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Trion.Client.Configs
+{
+    [Serializable]
+    internal class CMisc
+    {
+        #region Misc
+        public bool BunnyHop = true;
+        public bool AutoStrafe = true;
+        public bool MoonWalk = true;
+        #endregion
+    }
+}

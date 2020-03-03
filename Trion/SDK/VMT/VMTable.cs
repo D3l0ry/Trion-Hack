adf6fde7dde5cd0a17e05a75852542170a12c5da
Даниил <D3l0ry@outlook.com>
2020-03-03 16:20:12 +0500

    Optimization code initialize function and remove GetFreeMemory method

--- Trion/SDK/VMT/VMTable.cs
+++ Trion/SDK/VMT/VMTable.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-using System.Text;
 
-using Trion.SDK.Dumpers;
 using Trion.SDK.WinAPI;
 using Trion.SDK.WinAPI.Enums;
 using Trion.SDK.WinAPI.Structures;
-using Trion.SDK.WinAPI.Variables;
 
 namespace Trion.SDK.VMT
 {
     internal unsafe class VMTable
     {
         #region Variable
-        protected HANDLE Class_Base;
+        private uint** Class_Base;
 
         private uint* New_VMTable;
         private uint* Old_VMTable;
@@ -26,55 +23,42 @@ namespace Trion.SDK.VMT
 
         public static implicit operator VMTable(IntPtr Value) => new VMTable(Value);
 
-        public static explicit operator IntPtr(VMTable Value) => Value.Class_Base;
+        public static explicit operator IntPtr(VMTable Value) => (IntPtr)Value.Class_Base;
 
         public static implicit operator void*(VMTable Value) => Value.Class_Base;
-
-        public static implicit operator HANDLE(VMTable Value) => Value.Class_Base;
         #endregion
 
         #region Initializations
-        protected VMTable() { }
-
-        protected VMTable(void* Base, uint Module = 0)
+        protected VMTable(void* Base)
         {
-            if ((IntPtr)Base != IntPtr.Zero)
+            if (Base != IntPtr.Zero.ToPointer())
             {
-                Class_Base = Base;
+                Class_Base = (uint**)Base;
             }
 
-            if (Class_Base == IntPtr.Zero)
+            if (Class_Base == IntPtr.Zero.ToPointer())
             {
-                throw new ArgumentNullException("Class is NullPtr");
+                throw new NullReferenceException("Class is NullPtr");
             }
 
-            Old_VMTable = *(uint**)Class_Base;
+            Old_VMTable = *Class_Base;
 
-            VMTable_Length = VMTLength(Old_VMTable);
+            VMTable_Length = VMTLength(Old_VMTable, sizeof(uint));
 
             if (VMTable_Length == 0)
             {
                 throw new ArgumentNullException("VMTable Length is Null");
             }
 
-            if (Module == 0)
-            {
-                New_VMTable = (uint*)Marshal.AllocHGlobal((IntPtr)((VMTable_Length + 1) * sizeof(uint)));
-            }
-            else
-            {
-                New_VMTable = GetFreeMemory(Module, (VMTable_Length + 1) * sizeof(uint));
-            }
+            New_VMTable = (uint*)Marshal.AllocHGlobal((IntPtr)VMTable_Length);
 
-            uint Length = VMTable_Length * sizeof(uint);
-
-            Buffer.MemoryCopy(Old_VMTable, New_VMTable, Length, Length);
+            Buffer.MemoryCopy(Old_VMTable, New_VMTable, VMTable_Length, VMTable_Length);
 
             try
             {
                 Protect(Class_Base, sizeof(uint));
 
-                *(uint**)Class_Base = New_VMTable;
+                *Class_Base = New_VMTable;
             }
             catch
             {
@@ -84,7 +68,7 @@ namespace Trion.SDK.VMT
             }
         }
 
-        protected VMTable(IntPtr Base, uint Module = 0) : this(Base.ToPointer(), Module) { }
+        protected VMTable(IntPtr Base) : this(Base.ToPointer()) { }
         #endregion
 
         #region Hook Methods
@@ -94,39 +78,27 @@ namespace Trion.SDK.VMT
         #endregion
 
         #region Call Function Methods
-        protected T CallOriginalFunction<T>(int Index) => Marshal.GetDelegateForFunctionPointer<T>((HANDLE)Old_VMTable[Index]);
+        protected T CallOriginalFunction<T>(int Index) => Marshal.GetDelegateForFunctionPointer<T>((IntPtr)Old_VMTable[Index]);
 
-        protected T CallVirtualFunction<T>(int Index) => Marshal.GetDelegateForFunctionPointer<T>(Class_Base[Index]);
+        protected T CallVirtualFunction<T>(int Index) => Marshal.GetDelegateForFunctionPointer<T>((IntPtr)(*Class_Base)[Index]);
 
-        public static T CallVirtualFunction<T>(HANDLE Class, int Index) => Marshal.GetDelegateForFunctionPointer<T>(Class[Index]);
+        public static T CallVirtualFunction<T>(void* Class, int Index) => Marshal.GetDelegateForFunctionPointer<T>((IntPtr)(*(uint**)Class)[Index]);
         #endregion
 
         #region Helper
-        private uint VMTLength(uint* Table)
+        private uint VMTLength(uint* table, uint size)
         {
-            uint Length = 0U;
+            uint length = 0U;
 
-            while (NativeMethods.VirtualQueryEx(NativeMethods.GetCurrentProcess(), (IntPtr)Table[Length], out MEMORY_BASIC_INFORMATION MEMORY_INFORMATION, (uint)Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION))) != 0 && MEMORY_INFORMATION.Protect == AllocationProtect.PAGE_EXECUTE_READ)
+            while (NativeMethods.VirtualQuery((IntPtr)table[length], out MEMORY_BASIC_INFORMATION MEMORY_INFORMATION, Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION))) != 0 && MEMORY_INFORMATION.Protect == AllocationProtect.PAGE_EXECUTE_READ)
             {
-                Length++;
+                length++;
             }
 
-            return Length;
+            return length * size;
         }
 
-        private uint* GetFreeMemory(uint Module, uint VMTSize)
-        {
-            StringBuilder SearcherPattern = new StringBuilder();
-
-            for (uint Size = 0; Size < VMTSize; Size++)
-            {
-                SearcherPattern.Append("00 ");
-            }
-
-            return (uint*)(byte**)Pattern.FindPattern(Module, SearcherPattern.ToString().TrimEnd());
-        }
-
-        private bool Protect(void* Base, uint Size, ProtectCode ProtectCode = ProtectCode.PAGE_EXECUTE_READWRITE) => NativeMethods.VirtualProtect((HANDLE)Base, (HANDLE)Size, ProtectCode, out uint Old_Protect);
+        private bool Protect(void* Base, uint Size, ProtectCode ProtectCode = ProtectCode.PAGE_EXECUTE_READWRITE) => NativeMethods.VirtualProtect((IntPtr)Base, Size, ProtectCode, out uint Old_Protect);
         #endregion
     }
 }

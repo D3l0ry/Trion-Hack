b2869f9e2c35c5446c01ea83a2baa5d911e07fd0
Даниил <D3l0ry@outlook.com>
2020-04-10 00:46:49 +0500

    Update

--- Trion/EntryPoint.cs
+++ Trion/EntryPoint.cs
@@ -6,15 +6,15 @@ namespace Trion
 {
     internal unsafe class EntryPoint
     {
-        public static int DllMain(IntPtr hinstDLL, uint fdwReason, IntPtr lpvReserved)
+        public static int DllMain()
         {
-            Interface.Panel.Hook(41, Hooks.PaintTraverseDelegate);
+            //Interface.Panel.Hook(41, Hooks.PaintTraverseDelegate);
             Interface.ClientMode.Hook(24, Hooks.CreateMoveDelegate);
             Interface.GameEventManager.Hook(9, Hooks.FireEventClientSideDelegate);
             Interface.BaseClientDLL.Hook(37, Hooks.FrameStageNotifyDelegate);
-            Interface.ClientMode.Hook(44, Hooks.DoPostScreenEffectsDelegate);
+            //Interface.ClientMode.Hook(44, Hooks.DoPostScreenEffectsDelegate);
             Interface.NetVar.HookProp("DT_BaseViewModel", "m_nSequence", Hooks.SetViewModelSequenceDelegate, ref Interface.NetVar.SequencePtr);
-            Interface.Surface.Hook(67, Hooks.LockCursorDelegate);
+            //Interface.Surface.Hook(67, Hooks.LockCursorDelegate);
             Console.Beep();
             return 0;
         }
--- Trion/SDK/Structures/Intrinsics.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Trion.SDK.Structures
-{
-    internal static class Intrinsics
-    {
-        public static bool IsNull<T>(this T type) where T : unmanaged => typeof(T) == null;
-    }
-}
